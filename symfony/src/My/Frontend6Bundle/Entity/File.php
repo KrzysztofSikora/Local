7b4602b1edf2edf080789de641b88bfbafd83bce
krzysztof <listdlakrzycha@gmail.com>
2016-07-08 17:40:23 +0200

    image very well working

---
+++ symfony/src/My/Frontend6Bundle/Entity/File.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace My\Frontend6Bundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * File
+ *
+ * @ORM\Table(name="file")
+ * @ORM\Entity(repositoryClass="My\Frontend6Bundle\Repository\FileRepository")
+ */
+class File
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="filename", type="string", length=255, unique=true)
+     */
+    private $filename;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mime", type="string", length=255)
+     */
+    private $mime;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contents", type="text")
+     */
+    private $contents;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set filename
+     *
+     * @param string $filename
+     * @return File
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     * Get filename
+     *
+     * @return string 
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * Set mime
+     *
+     * @param string $mime
+     * @return File
+     */
+    public function setMime($mime)
+    {
+        $this->mime = $mime;
+
+        return $this;
+    }
+
+    /**
+     * Get mime
+     *
+     * @return string 
+     */
+    public function getMime()
+    {
+        return $this->mime;
+    }
+
+    /**
+     * Set contents
+     *
+     * @param string $contents
+     * @return File
+     */
+    public function setContents($contents)
+    {
+        $this->contents = $contents;
+
+        return $this;
+    }
+
+    /**
+     * Get contents
+     *
+     * @return string 
+     */
+    public function getContents()
+    {
+        return $this->contents;
+    }
+}
---
+++ symfony/src/My/Frontend7Bundle/MyFrontend7Bundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace My\Frontend7Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MyFrontend7Bundle extends Bundle
+{
+}
