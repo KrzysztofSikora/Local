da6a225e99273ff2ee0b319599d45c1905a838f5
krzysztof <listdlakrzycha@gmail.com>
2016-08-01 19:33:46 +0200

    displayer image

--- symfonyCRUDtest/src/MyTestBundle/Controller/DefaultController.php
+++ symfonyCRUDtest/src/MyTestBundle/Controller/DefaultController.php
@@ -36,12 +36,17 @@ class DefaultController extends Controller
 //            $image = $request->files ->all();
 //           print_r($image);
 //            $imagee = file_get_contents($image);
-            $imagee = (base64_encode(file_get_contents($image)));
+
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $name = (basename($image));
+            $mimeType = (finfo_file($finfo, $image));
+            $binary = (base64_encode(file_get_contents($image)));
+            finfo_close($finfo);
 
 
 
             return $this->render('MyTestBundle:Default:upload.html.twig', array ('owner' => $owner,
-                'description' => $description,'image' => $imagee));
+                'description' => $description, 'name'=> $name, 'mimeType' => $mimeType,'binary' => $binary));
 //            $response = new Response(
 //                'Content',
 //                Response::HTTP_OK,
