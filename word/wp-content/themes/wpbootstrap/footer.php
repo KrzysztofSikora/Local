7765b65e160b4ad6badbcf977bf9805a6e94e13a
krzysztof <listdlakrzycha@gmail.com>
2016-09-09 17:42:07 +0200

    panel edit

---
+++ word/wp-content/themes/wpbootstrap/footer.php
@@ -0,0 +1,13 @@
+
+     <hr>
+
+      <footer>
+        <p>Developed by Krzysztof Sikora © 2016</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
---
+++ word/wp-content/themes/wpbootstrap/header.php
@@ -0,0 +1,48 @@
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+  <div class="breadcrumb">
+
+
+
+      <h2 style="color: #c34f4f;text-align: center"><img src="/symfonyCRUDpanel/web/images/godło1.png" style="margin-right: 20px" width="40px">Miejska Orkiestra Dęta w Suchej Beskidzkiej
+      </h2>
+
+  </div>
+  <?php echo do_shortcode("[R-slider id='1']"); ?>
+  <div class="navbar navbar-inverse navbar">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+        <div class="nav-collapse collapse">
+          <ul class="nav">
+
+              <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
+
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
