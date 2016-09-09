77134fbfdf23a7de393e55b611113ec5340f4856
krzysztof <listdlakrzycha@gmail.com>
2016-09-09 14:36:40 +0200

    panel edit

--- word/wp-content/themes/twentyfourteen/footer.php
+++ word/wp-content/themes/twentyfourteen/footer.php
@@ -13,8 +13,38 @@
 		</div><!-- #main -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
+<!--ssss-->
+			<div class="container-fluid" id="footbar">
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-8">
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<div class="fb"><a
+									href="https://www.facebook.com/krzysztofsikora24pl-1541480686181605"><i
+										class="icon-facebook"></i></a>
+							</div>
+						</div>
 
-			<?php get_sidebar( 'footer' ); ?>
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<div class="yt"><a href="https://www.youtube.com"><i class="icon-youtube-squared"></i></a></div>
+						</div>
+
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<div class="tw"><a href="https://www.instagram.com"><i class="icon-instagram"></i></a></div>
+
+						</div>
+
+						<div class="col-xs-6 col-sm-4 col-md-3">
+							<div class="gplus"><a href="https://www.plus.google.com"><i class="icon-snapchat-square"></i></a></div>
+						</div>
+
+						<div style="clear: both"></div>
+					</div>
+					<div class="col-md-2"></div>
+
+				</div>
+<!--sss-->
+				<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info" style="text-align: center">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
--- word/wp-content/themes/twentyfourteen/header.php
+++ word/wp-content/themes/twentyfourteen/header.php
@@ -28,8 +28,28 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
-</head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<style>
+	#footbar > div.row > div.col-md-8 > div {
+		height: 50px;
+		background-color: chartreuse;
+	}
+
+
+	#footbar > .col-md-8 >  {
+		height: 120px;
+		background-color: red;
+	}
+
+</style>
 <body <?php body_class(); ?>>
 
 <div id="page" class="hfeed site">
